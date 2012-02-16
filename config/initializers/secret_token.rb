687e331cc4296f6abb914200e7d6bbf7d92cb242
Peter Shirley <peter.m.shirley@gmail.com>
2012-02-15 21:53:41 -0800

    Initial commit of sample "first app" from rails tutorial. This is almost exactly what rails new produced. * Modified .gitignore to ignore vim swap files. * Modified Gemfile to lock down gem versions.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'ecc70a07155281d9f3d7b9a0e509833e8883e5eede4e5cf3dcea704a44339447f6686e411359c770af02b7154f9e64a8c77a27a0820e5886c4d838609aadbf6f'
